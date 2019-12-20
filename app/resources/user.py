6fa80530dee6fff920cbb2e91d53d98e74394df8
Denis Maley <denis2digifit.eu>
2019-12-20 23:08:00 +0100

    Responce refactoring

--- app/resources/user.py
+++ app/resources/user.py
@@ -9,18 +9,15 @@ from models import UserModel
 class User(Resource):
     def get(self, username):
         user = UserModel.objects.get(username=username).to_json()
+
         return Response(user, mimetype="application/json", status=HTTPStatus.OK)
 
 
 class UserList(Resource):
     def post(self):
-        # Get posted data from request
         data = request.get_json()
-
         # encrypt password
         data["password"] = bcrypt.hashpw(data["password"].encode('utf8'), bcrypt.gensalt())
-
         user = UserModel(**data).save()
 
-        result = {"msg": "User was registered"}
-        return Response(result, mimetype="application/json", status=HTTPStatus.CREATED)
+        return {"id": str(user.id)}, HTTPStatus.CREATED
