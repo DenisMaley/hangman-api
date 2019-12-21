b702580864dade482f6839c4fe2fbfe41817605f
Denis Maley <denis2digifit.eu>
2019-12-21 22:59:09 +0100

    Added response statuses

--- app/resources/auth.py
+++ app/resources/auth.py
@@ -1,31 +1,33 @@
 from http import HTTPStatus
 
 from flask import request
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity)
 from flask_restful import Resource
 from models import UserModel
-from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required,
-                                jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+
 
 class Registration(Resource):
     def post(self):
         data = request.get_json()
 
         if UserModel.objects(username=data['username']):
-            return {'message': 'User already exists'}, HTTPStatus.BAD_REQUEST
+            return {'msg': 'User already exists'}, HTTPStatus.BAD_REQUEST
 
         try:
             # encrypt password
             data['password'] = UserModel.generate_hash(data['password'])
             user = UserModel(**data).save()
-            access_token = create_access_token(identity=data['username'])
-            refresh_token = create_refresh_token(identity=data['username'])
+            user_id = str(user.id)
+
+            access_token = create_access_token(identity=user_id)
+            refresh_token = create_refresh_token(identity=user_id)
             return {
-                'id': str(user.id),
-                'access_token': access_token,
-                'refresh_token': refresh_token,
-            }, HTTPStatus.CREATED
+                       'id': user_id,
+                       'access_token': access_token,
+                       'refresh_token': refresh_token,
+                   }, HTTPStatus.CREATED
         except:
-            return {'message': 'Something went wrong'}, HTTPStatus.INTERNAL_SERVER_ERROR
+            return {'msg': 'Something went wrong'}, HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 class Login(Resource):
@@ -33,7 +35,7 @@ class Login(Resource):
         data = request.get_json()
 
         if not UserModel.objects(username=data['username']):
-            return {'message': 'User {} doesn\'t exist'.format(data['username'])}
+            return {'msg': 'User {} doesn\'t exist'.format(data['username'])}, HTTPStatus.BAD_REQUEST
 
         current_user = UserModel.objects.get(username=data['username'])
 
@@ -41,19 +43,22 @@ class Login(Resource):
             access_token = create_access_token(identity=data['username'])
             refresh_token = create_refresh_token(identity=data['username'])
             return {
-                'message': 'Logged in as {}'.format(current_user['username']),
-                'access_token': access_token,
-                'refresh_token': refresh_token
-            }
+                       'msg': 'Logged in as {}'.format(current_user['username']),
+                       'access_token': access_token,
+                       'refresh_token': refresh_token
+                   }, HTTPStatus.OK
         else:
-            return {'message': 'Wrong credentials'}
+            return {'msg': 'Wrong credentials'}, HTTPStatus.UNAUTHORIZED
 
 
 class Logout(Resource):
     def post(self):
-        return {'message': 'User logout'}
+        return {'msg': 'User logout'}
 
 
 class TokenRefresh(Resource):
+    @jwt_refresh_token_required
     def post(self):
-        return {'message': 'Token refresh'}
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user)
+        return {'access_token': access_token}
