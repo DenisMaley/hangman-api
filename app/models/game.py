34e71888438bab10bc8b3a2ff91a960c1c5ab122
Denis Maley <denis2digifit.eu>
2019-12-25 23:05:45 +0100

    states introduction

--- app/models/game.py
+++ app/models/game.py
@@ -11,7 +11,7 @@ class GameModel(db.Document):
     author = db.ReferenceField(UserModel, required=True)
     word = db.ReferenceField(WordModel, required=True)
     named_letters = db.ListField()
-    lives = db.IntField(default=LIVES)
+    lives = db.IntField(min_value=0, max_value=LIVES, default=LIVES)
     start_timestamp = db.DateTimeField(default=datetime.datetime.now)
 
     def get_disguised_word(self):
@@ -24,6 +24,29 @@ class GameModel(db.Document):
                 symbols.append(PLACEHOLDER)
 
         return ' '.join(symbols)
+
+    def get_word_letters_set(self):
+        return set(list(self.word.value))
+
+    def get_named_letters_set(self):
+        return set(self.named_letters)
+
+    def is_succeeded(self):
+        return self.get_word_letters_set() <= self.get_named_letters_set()
+
+    def is_failed(self):
+        return self.lives == 0
+
+    def is_pending(self):
+        return self.lives > 0 and not self.is_succeeded()
+
+    def get_state(self):
+        if self.is_failed():
+            return 'FAILED'
+        elif self.is_succeeded():
+            return 'SUCCEEDED'
+        elif self.is_pending():
+            return 'IN PROGRESS'
 
     def make_turn(self, letter):
         letter = letter.upper()
@@ -40,4 +63,5 @@ class GameModel(db.Document):
             'lives': self.lives,
             'named_letters': self.named_letters,
             'disguised_word': self.get_disguised_word(),
+            'state': self.get_state(),
         }
