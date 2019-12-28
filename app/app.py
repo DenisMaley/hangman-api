eedcba419fe044ae4e7e37152501f0b2f6e4271e
Denis Maley <denis2digifit.eu>
2019-12-29 00:35:54 +0100

    swagger init commit

--- app/app.py
+++ app/app.py
@@ -3,10 +3,12 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
+from flasgger import Swagger
 from migrations import set_up_db
 from resources import initialize_routes
 
 app = Flask(__name__)
+Swagger(app)
 cors = CORS(app)
 api = Api(app, '/api')
 
--- app/resources/user.py
+++ app/resources/user.py
@@ -8,6 +8,29 @@ from models import UserModel
 class User(Resource):
     @jwt_required
     def get(self):
+        """
+            This is the User API
+            Call this api and get back logged in user
+            ---
+            tags:
+              - User API
+            responses:
+              500:
+                description: Internal Server Error
+              200:
+                description: A user with its details
+                schema:
+                  id: user
+                  properties:
+                    id:
+                      type: string
+                      description: The user id
+                      default: 5e07e5e5c3fc8412c95b53c1
+                    username:
+                      type: string
+                      description: username
+                      default: Denis
+        """
         user = UserModel.objects.get(pk=get_jwt_identity())
 
         return user.serialize(), HTTPStatus.OK
